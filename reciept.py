8459f4531791ef7a29503f634bf6f287d4ef6c45
Sam Whang <sangwoowhang@gmail.com>
2017-07-10 09:28:34 -0500

    Pep8 Reciept file

--- reciept.py
+++ reciept.py
@@ -10,7 +10,8 @@ import sys
 
 class Reciept(yaml.YAMLObject):
     """ Python Reciept using Yaml Object Parent for DB insertions """
-    yaml_tag=u'!Reciept'
+    yaml_tag = u'!Reciept'
+
     def __init__(self, *args):
         self.store = args[0]
         self.date = dt.date(args[1][0], args[1][1], args[1][2]).isoformat()
@@ -21,12 +22,14 @@ class Reciept(yaml.YAMLObject):
         self.tot = args[6]
         print(self.store, type(self.store))
         print(self.date, type(self.date))
-        self.code = self.store.replace(".","") + self.date
+        self.code = self.store.replace(".", "") + self.date
+
     def frmt(self, n):
         return "{:.2f}".format(n)
+
     def __repr__(self):
         return "{}({}: {})".format(self.__class__.__name__,
-                                        self.store, self.frmt(self.tot))
+                                   self.store, self.frmt(self.tot))
 
     def head(self):
         return self.store, self.date, self.type, self.code, \
