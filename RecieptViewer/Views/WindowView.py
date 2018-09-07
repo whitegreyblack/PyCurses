6cccf56088dd652402fa709ab9b6b50acad9d1d1
Sam Whang <sangwoowhang@gmail.com>
2018-09-07 15:12:59 -0700

    Basic wireframe for window

--- RecieptViewer/Views/WindowView.py
+++ RecieptViewer/Views/WindowView.py
@@ -9,20 +9,22 @@ def main(screen):
         screen_length = terminal_cols - 2
         screen_height = terminal_lines - 2
 
-        screen_divider = screen_height // 5 # closest int
-
+        horizontal_divider = (screen_height // 5) # closest int
+        vertical_divider = max(16, screen_height // 4) 
         curses.curs_set(0)
 
         # Mobile view
         screen.border()
-        screen.addstr(screen_height, 1, '-' * screen_length)
-        screen.getch()
-        
+        #screen.addstr(horizontal_divider, 1, '-' * screen_length)
+        for i in range(screen_height):
+            screen.addch(i + 1, vertical_divider, '|')
         # get bounds of header
-        
+        screen.addstr(0, 1, 'Reciept Viewer')
+
         # get bounds of main panel to add string
         
 
+        screen.getch()
     else:
         # Desktop view
         raise NotYetImplementedError()
