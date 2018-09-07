eadffc239b88484cb04a607760fed563177d07d8
Sam Whang <sangwoowhang@gmail.com>
2018-09-07 15:19:57 -0500

    Working on screen dividers between 'mobile mode' and 'desktop mode'

--- RecieptViewer/Views/WindowView.py
+++ RecieptViewer/Views/WindowView.py
@@ -1,15 +1,32 @@
 import curses
+def main(screen): 
+    terminal_lines = curses.LINES
+    terminal_cols = curses.COLS
 
-def main(screen):
-    screen.border()
-    screen.getch()
-    '''
-    y,x=screen.getmaxyx()
-    mypad=curses.newpad(y*3,9)
-    mypad.addstr(1,1,"here")
-    mypad.border()
-    mypad.refresh(0,0,0,0,y-1,9)
-    mypad.getch()
-    '''
+    if (curses.COLS <= 80):
+        # accounts for border characters taking up two lines horizontally and
+        # vertically each
+        screen_length = terminal_cols - 2
+        screen_height = terminal_lines - 2
+
+        screen_divider = screen_height // 5 # closest int
+
+        curses.curs_set(0)
+
+        # Mobile view
+        screen.border()
+        screen.addstr(screen_height, 1, '-' * screen_length)
+        screen.getch()
+        
+        # get bounds of header
+        
+        # get bounds of main panel to add string
+        
+
+    else:
+        # Desktop view
+        raise NotYetImplementedError()
+
 if __name__ == "__main__":
     curses.wrapper(main)
+    print(curses.LINES, curses.COLS)
