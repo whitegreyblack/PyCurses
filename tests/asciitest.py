743e2c64e07106021d2374ece09ceaa188e44aca
Sam Whang <sangwoowhang@gmail.com>
2018-09-21 20:29:24 -0700

    Moved some tests into test folder

--- tests/asciitest.py
+++ tests/asciitest.py
@@ -3,7 +3,7 @@ import curses.ascii
 def main(scr):
     for i in range(20):
         for j in range(15):
-            scr.addch(j,i,curses.ascii.alt(i*j))
+            scr.addch(j, i, curses.ascii.alt(i * j))
     scr.getch()
 
 if __name__ == "__main__":
