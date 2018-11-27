0a5671ec99c1ce697bd80b53f7d7e0ad63a90ef4
Sam Whang <sangwoowhang@gmail.com>
2018-11-27 11:23:07 -0600

    Added json example to board class

--- examples/storyboard.py
+++ examples/storyboard.py
@@ -25,13 +25,48 @@ Operations:
 
 FRONTEND: BLT - keyboard movement, keyboard actions. Mouse later.
 BACKEND: SQLite - there needs to be persistence every time program is opened.
+         
+Notes: Firstly use a text file to hold the DB. Possibly use JSON to hold info.
+       For db, will need a way to hold the board, story, task data with their
+       own separate tables.
 """
 import SQLite
 
 class Board(object):
     """Should initialize objects by calling database for story data"""
     def __init__(self):
+        self.column_todo = [] # these columns will be ordered by:
+        self.column_work = [] #     issue number asc;
+        self.column_done = [] #     date issued asc;
+
+        self.visible_todo = None
+        self.visible_work = None
+        self.visible_done = None
+
+    def import_json(self, filename):
+        """board example layout
+        {
+            {
+            'story 1': {
+                'column': 'work',
+                'subtask a': 'x',
+                'subtask b': 'y'
+            },
+            'story 2': {
+                'column': 'done',
+                'subtask a': 'z'
+            }
+        }
+        """
         pass
+
+    def import_database(self, tablename):
+        pass
+
+    def order_column(self, columnname):
+        newcolumnlist = sorted(getattr(self, columnname), 
+                               key=lambda x: x.created_date)
+        setattr(self, columnname.split("_")[1], newcolumnlist)
 
 class Story(object):
     def __init__(self):
