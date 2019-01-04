76428c4e729c80601472804b2771d9fbc1102dfb
Sam Whang <sangwoowhang@gmail.com>
2019-01-04 16:40:46 -0600

    Added name data files. Changed forms to windows inheritance

--- fakedata/forms.py
+++ fakedata/forms.py
@@ -3,28 +3,90 @@ forms.py:
 Includes Scrolling Window
 """
 from bearlibterminal import terminal as t
-
+from itertools import chain
 DEF_X = 0
 DEF_Y = 0
 MAX_X = 80
 MAX_Y = 24
 
-class Form(object):
-    def __init__(self, x=DEF_X, y=DEF_Y, mx=MAX_X, my=MAX_Y):
+class ScrollFormModel:
+    def __init__(self, topic, info):
+        self.topic = topic
+        self.info = info
+    def __str__(self):
+        return self.topic
+
+class Window(object):
+    def __init__(self, x=DEF_X, y=DEF_Y, mx=MAX_X, my=MAX_Y, showing=True):
+        # outer bounding box
         self.x = x
         self.y = y
+        self.mx = mx
+        self.my = my
+
+        # inner bounding box -- possibly modify based on inputs/args
         self.sx = x + 1
         self.xy = y + 1
         self.ex = mx - 1
         self.ey = my - 1
-        self.mx = mx
-        self.my = my
+
+        # object box properties
         self.width = self.mx - self.x
         self.height = self.my - self.y
-        print(f"Height: {self.height}")
+        
+        # view/visual properties
+        self.showing = showing
+
+        # child objects
+        self.texts = []
+
+    def __repr__(self):
+        return f"Window({self.width}, {self.height})"
+
+    @property
+    def form(self):
+        mx = self.mx - 2
+        # character map
+        rows = []
+
+        # border map
+        for y in range(self.my+1):
+            e, d = "|", " "
+            if y in (0, self.my):
+                e, d = "+", "-"
+            rows.append(f"{e}{d*mx}{e}")
+    
+        for x, y, text in self.texts:
+            rows[y] = f"|{text.rjust(x+len(text)).ljust(mx)}|"
+
+        return '\n'.join(rows)
+
+    @property
+    def forms(self):
+        yield self.x, self.y, self.form
+
+class Form(object):
+    def __init__(self, x=DEF_X, y=DEF_Y, mx=MAX_X, my=MAX_Y, showing=True):
+        # outer bounding box
+        self.x = x
+        self.y = y
+        self.mx = mx
+        self.my = my
+        
+        self.sx = x + 1
+        self.xy = y + 1
+        self.ex = mx - 1
+        self.ey = my - 1
+        
+        self.width = self.mx - self.x
+        self.height = self.my - self.y
+        
+        self.showing = showing
+        self.texts = []
 
     def __repr__(self):
         return f"Form({self.width}, {self.height})"
+
     @property
     def form(self):
         mx = self.mx - 2
@@ -35,24 +97,89 @@ class Form(object):
                 e, d = "+", "-"
             rows.append(f"{e}{d*mx}{e}")
         return '\n'.join(rows)
-class ScrollForm(Form):
-    def __init__(self):
-        super().__init__()
+
+    @property
+    def forms(self):
+        yield self.x, self.y, self.form
+
+class Text(Window):
+    SCROLLABLE = 1
+    TEXTWRAP = 2
+    BORDERED = 4
+    def __init__(self, x=DEF_X, y=DEF_Y, mx=MAX_X, my=MAX_Y, text=None):
+        super().__init__(x, y, mx, my)
         self.index = 0
-        self.rows = []
+        self.text = text if text else None
+
+    @property
+    def form(self):
+        return self.text
+
+    @property
+    def forms(self):
+        self.x, self.y, self.form
+
+class ScrollForm(Window):
+    def __init__(self, x=DEF_X, y=DEF_Y, mx=MAX_X, my=MAX_Y, rows=None):
+        super().__init__(x, y, mx, my)
+        self.index = 0
+        self.rows = rows if rows else []
+        self.showing = True
+        self.subwin = None
+        self.texts = []
+
+    def change_subwin(self):
+        if self.subwin.showing:
+            self.close_subwin()
+        else:
+            self.open_subwin()
+
+    def open_subwin(self):
+        self.subwin.showing = True
+        self.subwin.texts = [(3, 1, self.rows[self.index].info)]
+    
+    def close_subwin(self):
+        self.subwin.showing = False
+
     def add_row(self, row):
         self.rows.append(row)
+        self.subwin.showing = True
+        self.subwin.texts = [(3, 1, self.rows[self.index].info)]
+
     def sub_row(self, index=-1):
+        """
+        >>> import forms
+        >>> f = forms.ScrollForm()
+        >>> f.sub_row()
+        >>> f.subwin.showing
+        False
+        >>> f.add_row("test row")
+        >>> f.subwin.showing
+        True
+        >>> f.rows
+        ['test row']
+        >>> f.sub_row()
+        >>> f.rows
+        []
+        """
         if self.rows:
             self.rows.pop(index)
+
+        if not self.rows:
+            self.subwin.showing = False
+
     def inc_index(self):
         within_bounds = self.index + 1 < len(self.rows)
         if self.rows and within_bounds:
             self.index += 1
+            self.subwin.texts = [(3, 1, self.rows[self.index].info)]
+
     def dec_index(self):
         within_bounds = self.index - 1 >= 0
         if self.rows and within_bounds:
             self.index -= 1
+            self.subwin.texts = [(3, 1, self.rows[self.index].info)]
+
     @property
     def form(self):
         mx = self.mx - 2
@@ -79,10 +206,22 @@ class ScrollForm(Form):
             s = 0
             rows_in_view = self.rows
         for i, r in enumerate(rows_in_view):
-            rows[i+1] = f"|{r.ljust(mx)}|"
+            rows[i+1] = f"|{str(r).ljust(mx)}|"
             if self.index == s + i:
-                rows[i+1] = f"|[color=orange]{r.ljust(mx)}[/color]|"
+                rows[i+1] = f"|[color=orange]{str(r).ljust(mx)}[/color]|"
+
+        for x, y, text in self.texts:
+            rows[y] = f"|{text.rjust(x + len(text)).ljust(mx)}|"
+
         return '\n'.join(rows)
+
+    @property
+    def forms(self):
+        yield self.x, self.y, self.form
+
+        if self.rows and self.subwin.showing:
+            for x, y, form in self.subwin.forms:
+                yield x, y, form
 
 if __name__ == "__main__":
     f = Form()
@@ -91,14 +230,6 @@ if __name__ == "__main__":
     t.puts(f.x, f.y, f.form)
     t.refresh()
     t.read()
-    del f
-    s = ScrollForm()
-    for i in range(50):
-        s.add_row(f"row {i}")
-    t.puts(s.x, s.y, s.form)
-    t.refresh()
-    t.read()
-    s.index = 25
-    t.puts(s.x, s.y, s.form)
-    t.refresh()
-    t.read()
+
+    import doctest            
+    doctest.testmod()
--- fakedata/scrollform_ex.py
+++ fakedata/scrollform_ex.py
@@ -1,16 +1,18 @@
 """Scrollform example"""
 from bearlibterminal import terminal as t
-from forms import ScrollForm
+from forms import ScrollForm, ScrollFormModel, Form, Window
+from itertools import chain
 
 # blt keycodes to break loop
 exit_codes = [t.TK_ESCAPE, t.TK_CLOSE, t.TK_Q]
 
 # instructions on using demo
 directions = [
-    "<a>    - Add line",
-    "<s>    - Remove last line",
-    "<DOWN> - Move index pointer down one",
-    "<UP>   - Move index pointer up one"
+    "<a>     - Add line",
+    "<s>     - Remove last line",
+    "<DOWN>  - Move index pointer down one",
+    "<UP>    - Move index pointer up one",
+    "<ENTER> - View topic text"
     ]
 
 # change to string type to be able to print to term
@@ -18,21 +20,30 @@ printable_directions = "\n".join(directions)
 
 # the 'scrollable object
 s = ScrollForm()
+s.subwin = Window(x=35, y=0, mx=45, my=12, showing=False)
+for i, d in enumerate(directions):
+    s.texts.append((36, 13 + i, d))
 
 user_mapping = {
-    t.TK_A: lambda x: x.add_row(f"added row {len(x.rows) + 1}"),
+    t.TK_A: lambda x: x.add_row(
+        ScrollFormModel(
+            f"added row {len(x.rows) + 1}", 
+            f"text for row {len(x.rows) + 1}"
+        )),
     t.TK_S: lambda x: x.sub_row(),
     t.TK_UP: lambda x: x.dec_index(),
     t.TK_DOWN: lambda x: x.inc_index(),
+    t.TK_ENTER: lambda x: x.change_subwin(),
     }
 
 # main demo
 t.open()
 while True:
-    t.puts(s.x, s.y, s.form)
-    t.puts(1, 0, str(s))
+    for x, y, rows in s.forms:
+        t.puts(x, y, rows)
+    t.puts(1, 23, str(s))
     t.puts(15, 0, f"I: {s.index}, Rows: {len(s.rows)}")
-    t.puts(40, 2, printable_directions)
+    # t.puts(40, 2, printable_directions)
     t.refresh()
     ch = t.read()
     if ch in exit_codes:
