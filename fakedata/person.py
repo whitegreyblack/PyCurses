85a2201ff3ebe509becf21c3cedffd9b885d2ae3
Sam Whang <sangwoowhang@gmail.com>
2019-01-07 12:04:11 -0600

    Working on random person name generator

--- fakedata/person.py
+++ fakedata/person.py
@@ -1,24 +1,109 @@
 import random
 
-names_males = set()
-names_females = set()
-names_males_ordered = []
-names_females_ordered = []
+def read_file(dataset):
+    container = []
+    with open(f'./data/{dataset}.txt', 'r') as data:
+        for line in data.readlines():
+            container.append(line.strip())
+    return container, set(container)
 
-with open('./data/NAMES_MALE.txt', 'r') as names_m:
-    for line in names_m.readlines():
-        names_males.add(line.strip())
+names_last_ordered, names_last = read_file('NAMES_LAST')
+names_males_ordered, names_males = read_file('NAMES_MALE')
+names_females_ordered, names_females = read_file('NAMES_FEMALE')
+# names_males_prefix = set()
+# names_males_suffix = set()
+# names_females_prefix = set()
+# names_females_suffix = set()
 
-with open('./data/NAMES_FEMALE.txt', 'r') as names_f:
-    for line in names_f.readlines():
-        names_females.add(line.strip())
+# names_males_prefix_ordered = []
+# names_males_suffix_ordered = []
+# names_females_prefix_ordered = []
+# names_females_suffix_ordered = []
+
+# with open('./data/NAMES_MALE.txt', 'r') as names_m:
+#     for line in names_m.readlines():
+#         names_males.add(line.strip())
+
+# with open('./data/NAMES_FEMALE.txt', 'r') as names_f:
+#     for line in names_f.readlines():
+#         names_females.add(line.strip())
+
+# with open('./data/NAMES_LAST.txt', 'r') as names_l:
+#     for line in names_l.readlines():
+#         names_last.add(line.strip())
 
 class Person:
-    def __init__(self, name, age, phone, address):
-        self.name = name
-        self.age = age
-        self.phonenumber = phone
-        self.address = address
+    names_last = names_last_ordered
+    names_males = names_males_ordered
+    names_females = names_females_ordered
+    names_females_prefix = ('Mrs.', 'Miss')
+    formats = (
+        # ('male', ('prefix', 'first', 'last', 'suffix')),
+        # ('male', ('prefix', 'first', 'last')),
+        # ('male', ('first', 'last', 'suffix')),
+        # ('male', ('first', 'last')),
+        ('female', ('prefix', 'first', 'last', 'suffix')),
+        ('female', ('prefix', 'first', 'last')),
+        ('female', ('first', 'last', 'suffix')),
+        ('female', ('first', 'last'))
+    )
+    def __init__(self, first=None, last=None, prefix=None, suffix=None):
+        self.name_first = first if first else ''
+        self.name_last = last if last else ''
+        self.name_prefix = prefix if prefix else ''
+        self.name_suffix = suffix if suffix else ''
+
+    def __str__(self):
+        p = self.name_prefix
+        f = self.name_first
+        l = self.name_last
+        s = self.name_suffix
+        return f"{p}{' ' if p else ''}{f} {l}{' ' if s else ''}{s}"
+
+    def prefix(self, gender):
+        if gender == "male":
+            return male_prefix()
+        return self.female_prefix()
+
+    def first(self, gender):
+        if gender == "male":
+            return male_first()
+        return self.female_first()
+    
+    def last(self, gender):
+        if gender == "male":
+            return male_last()
+        return self.female_last()
+
+    def suffix(self, gender):
+        if gender == "male":
+            return male_suffix()
+        return self.female_suffix()
+
+    def male_prefix(self):
+        if hasattr(self, 'male_prefixes'):
+            return random.choice(self.male_prefixes)
+        return ''
+
+    def female_prefix(self):
+        if hasattr(self, 'female_prefixes'):
+            return random.choice(self.female_prefixes)
+        return ''
+
+    def female_first(self):
+        if hasattr(self, 'names_females'):
+            return random.choice(self.names_females)
+        return random.choice(self.names_first)
+
+    def female_last(self):
+        if hasattr(self, 'names_females_last'):
+            return random.choice(self.female_last)
+        return random.choice(self.names_last)
+    
+    def female_suffix(self):
+        if hasattr(self, 'female_suffix'):
+            return random.choice(self.female_suffix)
+        return ''
 
     @classmethod
     def random(cls):
@@ -26,8 +111,19 @@ class Person:
         #       A random person is still faraway from where the generator is
         #       currently at. We would need at least random name, age and 
         #       phone number to create a basic person object.
-        return cls(None, None, None, None)
+        # TODO: Could also rename to generate()
+        p = Person()
+        gender, choices = random.choice(Person.formats)
+        for choice in choices:
+            print(choice, gender)
+            attr = getattr(p, choice)(gender)
+            print(attr)
+            setattr(p, 'name_'+choice, attr)
+        return p
 
 if __name__ == "__main__":
+    print(names_last)
     print(names_males)
     print(names_females)
+    print(Person.formats)
+    print(Person.random())
