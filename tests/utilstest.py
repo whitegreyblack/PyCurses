37a9c757e34d12f7b1eadd71d5f56f8e00238639
Sam Whang <sangwoowhang@gmail.com>
2019-01-08 16:49:58 -0600

    Added util tools to validate yaml files

--- tests/utilstest.py
+++ tests/utilstest.py
@@ -52,3 +52,16 @@ def test_parse_date_from_database():
 
 def test_format_date():
     pass
+
+def test_load_yaml_object_document():
+    o = utils.load_yaml_object('./data/store.yaml', doc=True)
+    assert o['store'] == 'Leevers'
+
+def test_load_yaml_object_schema():
+    o = utils.load_yaml_object('./data/schema.yaml')
+    assert isinstance(o['store'], dict)
+
+def test_validate_document_schema():
+    d = utils.load_yaml_object('./data/store.yaml', doc=True)
+    s = utils.load_yaml_object('./data/schema.yaml')
+    assert utils.validate(d, s) == True
