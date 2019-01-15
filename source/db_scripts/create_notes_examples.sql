0a2fb3e0cd9a9ed6b61528c73a0929c867e3ef4d
Sam Whang <sangwoowhang@gmail.com>
2019-01-15 11:07:56 -0600

    Modified notes sql scripts to separate creation and example files

---
+++ source/db_scripts/create_notes_examples.sql
@@ -0,0 +1,33 @@
+drop table if exists notes;
+
+create table notes (
+    id_note integer PRIMARY KEY AUTOINCREMENT,
+    title varchar(20),
+    created timestamp,
+    modified timestamp,
+    note varchar(250)
+);
+
+insert into notes (
+    title,
+    created,
+    modified,
+    note
+) values (
+    'example 1',
+    datetime('now'),
+    datetime('now'),
+    'These are example notes for note 1'
+);
+
+insert into notes (
+    title,
+    created,
+    modified,
+    note
+) values (
+    'example 2',
+    datetime('now'),
+    datetime('now'),
+    'ali baba had a great meal for those with small bellies.'
+)
