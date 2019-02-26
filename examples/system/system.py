519bd52a38d78a2d19d7e869b042f6043cafe04e
Sam Whang <sangwoowhang@gmail.com>
2019-02-26 10:07:34 -0600

    Working base cases on system tree view

--- examples/system/system.py
+++ examples/system/system.py
@@ -15,7 +15,21 @@ End Goal:
     |__ out_of_place.png
     |__ verylongfilename_..._.png
 """
-
+data2 = [
+    ("Documents", [
+        ("Projects", [
+            # "File1.png",
+            # "File2.txt",
+        ]),
+        ("Games", []),
+    ]),
+    ("Music", []),
+    ("Pictures", []),
+    ("Desktop", [
+        ("Stuff", [])
+    ]),
+    "Out_Of_Place_File.txt",
+]
 
 data = [
     ("Documents", [
@@ -42,13 +56,13 @@ class Directory(object):
         self.parent = parent
         self.folders = []
         self.files = []
-        self.children = []
+        self.children = {}
 
         for sysobj in sysobjs:
             if isinstance(sysobj, tuple):
                 name, children = sysobj
                 self.folders.append(SystemObject.Folder(name))
-                self.children.append(Directory(self, children))
+                self.children[name] = Directory(self, children)
 
             if isinstance(sysobj, str):
                 self.files.append(SystemObject.File(sysobj))
@@ -61,28 +75,52 @@ class Directory(object):
 
     def print_tree(self, level=0):
         # fix level spacing
-        enum_end = len(self.folders)
+        def get_spacing(level):
+            return max(level, 0) * 2
 
-        for i, f in enumerate(self.folders):
-            if enum_end == i+1 and len(self.files) == 0:
-                addr = u'\u2502' + u'\u0020' * ((level+1) // 2) + u'\u2514\u2500'
-            elif level == 0:
-                addr = u'\u251C\u2500'
+        enum_end = len(self.folders)
+        for i, f in enumerate(sorted(str(folder) for folder in self.folders)):
+            addr = ""
+                # addr = u'\u2502' + u'\u0020' * get_spacing(level) + u'\u2514\u2500'
+            if level == 0:
+                if enum_end == i+1 and len(self.files) == 0:
+                    addr = u'\u2514'
+                else:
+                    addr = u'\u251C'
+                addr += u'\u2500\u0020'
             else:
-                addr = u'\u2502' + u'\u0020' * ((level+1) // 2) + u'\u251C\u2500'
+                if len(self.files) == 0:
+                    addr += u'\u0020'
+                else:
+                    addr += u'\u2502'
+                addr += u'\u0020' * get_spacing(level)
+
+                if enum_end == i+1:
+                    addr += u'\u2514'
+                else:
+                    addr += u'\u251C'
+                addr += u'\u2500\u0020'
+
+            # print(addr + str(f))
             print(addr + str(f))
-            
-            if len(self.children) > i:
-                self.children[i].print_tree(level+1)
+
+            children = self.children.get(f[:-1], None)
+            if children:
+                children.print_tree(level+1)
 
         enum_end = len(self.files)
         for i, f in enumerate(self.files):
-            if enum_end == i+1:
-                addr = u'\u2502' + u'\u0020' * ((level+1) // 2) + u'\u2514\u2500'
-            elif level == 0:
-                addr = u'\u251C\u2500'
-            else:
-                addr = u'\u2502' + u'\u0020' * ((level+1) // 2) + u'\u251C\u2500'
+            # if enum_end == i+1:
+            #     addr = u'\u2502' + u'\u0020' * ((level+1) // 2) + u'\u2514\u2500'
+            if level == 0:
+                if enum_end == i + 1:
+                    addr = u'\u2514'
+                else:
+                    addr = u'\u251C'
+                addr += u'\u2500\u0020'
+            # else:
+            #     addr = u'\u2502' + u'\u0020' * ((level+1) // 2) + u'\u251C\u2500'
+            # print(addr+str(f))
             print(addr+str(f))
 
 class SystemObject(object):
@@ -110,7 +148,14 @@ class SystemObject(object):
 if __name__ == "__main__":
     import pprint as p
 
-    # for d in data:
-    #     p.pprint(d)
+    for i in range(4):
+        print(max(i, 0)*2)
 
-    Directory(sysobjs=data).print_tree()
+    print('2514', '\u2514')
+    print('2500', '\u2500')
+    print('2502', '\u2502')
+    print('251C', '\u251c')
+    print('0020', '\u0020')
+    print('a' + '\u0020\u0020' + 'b')
+
+    Directory(sysobjs=data2).print_tree()
