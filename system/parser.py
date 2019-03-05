dc53025f715aae4088282192eef6ec646807ba3a
Sam Whang <sangwoowhang@gmail.com>
2019-03-05 16:54:15 -0600

    Adding init to hold helper functions used across all system files

--- system/parser.py
+++ system/parser.py
@@ -1,6 +1,6 @@
 # parser.py
 import yaml
-from collections import namedtuple
+from system import *
 """
 Documents:
   - File
@@ -38,8 +38,6 @@ dict(list(dict(list())))
 
 filepath = "system\structure.yaml"
 node = namedtuple("Node", "nid gid pid cid name")
-dirsort = lambda x: (x.cid is None, x.name)
-dirfilter = lambda l, i: list(filter(lambda x: x.gid == i, l))
 
 def parse(filepath):
     with open(filepath, 'r') as f:
@@ -47,16 +45,13 @@ def parse(filepath):
     return yaml.safe_load(data)
 
 def deserialize(data, i=0, sublevel=1):
-    t = []
-    s = []
-    # print('n g p c')
+    s, t = [], []
     for k, v in data.items():
         s.append((k, v, 0, i, '$'))
         i += 1
         while s:
             k, v, l, j, p = s.pop(0)
             if v is not None:
-                # print(j, l, p, sublevel, k)
                 t.append(node(j, l, p, sublevel, k+"/"))
                 if isinstance(v, list):
                     for f in v:
@@ -69,86 +64,16 @@ def deserialize(data, i=0, sublevel=1):
                                 i += 1
                 sublevel += 1
             else:
-                # print(j, l, p, sublevel, k)
                 t.append(node(j, l, p, None, k))
-    # print()
-    # for n in t:
-    #     print(el_repr(n))
-    # print()
     return t
-
-def els_repr(ns):
-    return [el_repr(n) for n in ns]
-
-def el_repr(n):
-    return f"{n.nid} {n.gid} {n.cid if n.cid else '|'} {n.name}"
-
-def elements(t, i):
-    elms = sorted(dirfilter(t, i), key=dirsort, reverse=True)
-    # print([el_repr(e) for e in elms])
-    return elms
-
-def print_inorder(t):
-    *ns, n = elements(t, 0)
-    print('n g c')
-    while n:
-        print(el_repr(n))
-        if n.cid:
-            for e in elements(t, n.cid):
-                ns.append(e)
-            # print([el_repr(n) for n in ns])
-        n = None
-        if ns:
-            n = ns.pop()
-    # for n in ns:
-    #     print(n.nid, n.cid, n.name)
-
-def deserialize_inorder_group(data):
-    # initial iteration list
-    l = []
-    index = 0
-    level = 0
-    d = [(k, v, level) for k, v in data.items()]
-    while d:
-        k, v, l = d.pop(0)
-        print(l, k)
-        if v and isinstance(v, list):
-            for f in v:
-                if isinstance(f, str):
-                    d.append((f, None, l+1))
-                else:
-                    for fk, fv in f.items():
-                        d.append((fk, fv, l+1))
-        elif v and isinstance(v, dict):
-            for vk, vv in v.items():
-                d.append((vk, vv, l+1))
-
-def deserialize_inorder_level(data):
-    # initial iteration list
-    l = []
-    level = 0
-    d = [(k, v, level) for k, v in data.items()]
-    while d:
-        k, v, l = d.pop(0)
-        print(l, k)
-        if v and isinstance(v, list):
-            for f in v:
-                if isinstance(f, str):
-                    d.insert(0, (f, None, l+1))
-                else:
-                    for fk, fv in f.items():
-                        d.insert(0, (fk, fv, l+1))
-        elif v and isinstance(v, dict):
-            for vk, vv in v.items():
-                d.insert(0, (vk, vv, l+1))
 
 def serialize_list(data):
     pass
 
-def deserialize_hash(data):
+def serialize_hash(data):
     pass
 
-def serialize_hash(data):
+def serialize_hashlist(data):
     pass
 
 def to_hashsys(t):
@@ -168,24 +93,25 @@ def to_hashlistsys(t):
     return d, l
 
 if __name__ == "__main__":
-    print("""
-    Documents/:
-    - File
-    - Folder/:
-        - Text
-    - Directory/:
-        - Image
-    Music/:
-    - Playlist/
-    - Favorites/: []"""[1:])
+    # print("""
+    # Documents/:
+    # - File
+    # - Folder/:
+    #     - Text
+    # - Directory/:
+    #     - Image
+    # Music/:
+    # - Playlist/
+    # - Favorites/: []"""[1:])
     structure = parse(filepath)
-    print(structure)
+    # print(structure)
     
     t = deserialize(structure)
+    # print_inorder(t)
+    print_inorder_indent_tree(t)
 
-    print_inorder(t)
-
-    to_hashsys(t)
-
-    d, l = to_hashlistsys(t)
-    print(d)
+    # d = to_hashsys(t)
+    # print(d)
+    
+    # d, l = to_hashlistsys(t)
+    # print(d)
