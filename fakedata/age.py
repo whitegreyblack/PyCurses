7fcd4a9510ab409d5667643c2dc34f2cf8a41c3e
whitegreyblack <sangwoowhang@gmail.com>
2020-04-12 20:30:12 -0700

    Added check for non-negative input

--- fakedata/age.py
+++ fakedata/age.py
@@ -6,9 +6,17 @@ import random
 
 
 class Age(int):
+    """
+    Age class for all non-negative values
+    >>> Age(3)
+    Age(value=3)
+    """
     def __init__(self, years):
+        if years < 0:
+            raise ValueError(f"Years cannot be less than 0. Got: {years}")
         self = years
-
+    def __repr__(self):
+        return f"{self.__class__.__name__}(value={self})"
     @classmethod
     def random(cls, low=1, high=100):
         return cls(random.randint(low, high))
@@ -20,6 +28,8 @@ if __name__ == "__main__":
     elif sys.argv[1] == '--test':
         import doctest
         doctest.testmod()
+    elif int(sys.argv[1]):
+        print(Age(int(sys.argv[1])))
     else:
         print("""
 age.py
